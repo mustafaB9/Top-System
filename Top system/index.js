dc84530dd518731c2a7cf3111f10d82c342a3616
mustafaB9 <127650070+mustafaB9@users.noreply.github.com>
2023-06-08 10:58:32 -0400

    Add files via upload

---
+++ Top system/index.js
@@ -0,0 +1,16 @@
+// let open = Document.getElementById("open")
+// let close = Document.getElementById("close")
+// let dash = Document.getElementById("dash")
+
+// open.onClick=addClass();
+
+//   // add class on click javascript
+//   function addClass() {
+//     dash.classList.add("open-dash")
+//   }
+
+// close.onClick=removeClass()
+
+// function removeClass() {
+//     dash.classList.add("close-dash")
+//   }
